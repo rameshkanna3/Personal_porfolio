59e496e8d3265a1d0f1b5c59694baa1ac0f9dd79
ramesh kanna <srameshkanna3@gmail.com>
2023-03-26 23:59:33 +0530

    completed

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-let hamMenuIcon = document.getElementById("ham-menu");
-let navBar = document.getElementById("nav-bar");
-let navLinks = navBar.querySelectorAll("li");
-
-hamMenuIcon.addEventListener("click", () => {
-  navBar.classList.toggle("active");
-  hamMenuIcon.classList.toggle("fa-times");
-});
-navLinks.forEach((navLinks) => {
-  navLinks.addEventListener("click", () => {
-    navBar.classList.remove("active");
-    hamMenuIcon.classList.toggle("fa-times");
-  });
-});
-
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+  sidemenu.style.right="0";
+}
+function closemenu(){
+  sidemenu.style.right="-200px";
+}
 // ----------About Section----------
 
 var tablinks=document.getElementsByClassName("tab-links");
@@ -28,3 +20,21 @@ function opentab(tab){
   window.event.currentTarget.classList.add("active-link");
   document.getElementById(tab).classList.add("active-tab");
 }
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwtiD_FhO4uDixvVCQACVebleB2o3haQ0rODio_mbZDU3Tq-4j22P_e0pdhJVErm19leg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+         msg.innerHTML="Message Sent Successfully..!"
+         setTimeout(function(){
+          msg.innerHTML=""
+         },5000)
+         form.reset()
+        })
+      .catch(error => console.error('Error!', error.message))
+  })
